a9147a63a29469c64f15527a0446874a47c38ff7
Harkirats043 <Harkirats043@gmail.com>
2023-09-22 15:01:05 -0400

    update admin

--- main_app/views.py
+++ main_app/views.py
@@ -3,37 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def get_finch_data():
     finches = [
-        {
-            'name': 'Zebra Finch',
-            'scientific_name': 'Taeniopygia guttata',
-            'description': 'Small, colorful bird native to Australia.',
-            'habitat': 'Open grasslands, savannas, and forests.'
-        },
-        {
-            'name': 'Gouldian Finch',
-            'scientific_name': 'Erythrura gouldiae',
-            'description': 'Brightly colored bird with distinctive head colors.',
-            'habitat': 'Northern Australia, particularly in savannas.'
-        },
-         {
-            'name': 'Java Sparrow',
-            'scientific_name': 'Lonchura oryzivora',
-            'description': 'Small bird with gray plumage and pink beak.',
-            'habitat': 'Grasslands, agricultural fields, and urban areas of Java.'
-        },
-        {
-            'name': 'Society Finch',
-            'scientific_name': 'Lonchura domestica',
-            'description': 'Small and social bird known for its singing ability.',
-            'habitat': 'Originally from Southeast Asia, now kept as a pet worldwide.'
-        },
-        {
-            'name': 'Star Finch',
-            'scientific_name': 'Neochmia ruficauda',
-            'description': 'Colorful bird with red head and tail.',
-            'habitat': 'Open woodlands, grasslands, and scrublands of Australia.'
-        }
-    ]
+    {'name': 'Canary Finch', 'color': 'Yellow', 'size': 'Small'},
+    {'name': 'Bengalese Finch', 'color': 'Brown', 'size': 'Small'},
+    {'name': 'Diamond Firetail Finch', 'color': 'Multicolored', 'size': 'Small'},
+    {'name': 'Owl Finch', 'color': 'White and Gray', 'size': 'Small'},
+    {'name': 'Zebra Finch', 'color': 'Gray', 'size': 'Small'}
+]
     return finches
 
 def home(request):
